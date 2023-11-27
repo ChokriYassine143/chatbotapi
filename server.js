e00e114f758c7b70ecdd9a142e0e6b9b642f9ce4
Chokri Yassine <123024506+ChokriYassine143@users.noreply.github.com>
2023-11-27 20:54:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,45 @@
-const express=require("express");
+const express = require("express");
 const bodyParser = require('body-parser');
-
-const axios=require("axios");
-const app=express();
+const axios = require("axios");
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 require('dotenv').config();
 const apiKey = process.env.API_KEY;
-app.get("/chatbot/",async (req,res)=>{
-  const prompt = req.body.prompt;
+
+app.get("/chatbot/", async (req, res) => {
+  const prompt = req.query.prompt; // Change to req.query.prompt
+
+  if (!prompt) {
+    return res.status(400).send("Missing 'prompt' parameter.");
+  }
 
   const requestData = {
     prompt: {
-      text:prompt,
+      text: prompt,
     },
   };
 
-  
-  apiUrl="https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText"
-  axios.post(apiUrl, requestData, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    params: {
-      key: apiKey,
-    },
-  })
-    .then(response => {
-      console.log('Response:', response.data);
-      res.send(response.data.candidates[0].output);
-    })
-    .catch(error => {
-      console.error('Error:', error.response ? error.response.data : error.message);
-    });
-})
-app.listen(process.env.PORT ||3000,()=>{
+  const apiUrl = "https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText";
 
-  console.log("working in port 3000");
-})
+  try {
+    const response = await axios.post(apiUrl, requestData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      params: {
+        key: apiKey,
+      },
+    });
+
+    console.log('Response:', response.data);
+    res.send(response.data.candidates[0].output);
+  } catch (error) {
+    console.error('Error:', error.response ? error.response.data : error.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port 3000");
+});
